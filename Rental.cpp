dbac0964ba8fd49b9b0bf901633ef6f6dc33da85
ionelmasca <ionel_masca@yahoo.com>
2020-03-30 23:43:40 +0300

    Update Rental.cpp
    delete, display,  update

--- Rental.cpp
+++ Rental.cpp
@@ -27,3 +27,39 @@ void Rental::add_client(Kunde a)
 {
 	v.push_back(a);
 }
+
+void Rental::delete_client(Kunde a)
+{
+	int i = 0;
+	while (i < v.size())
+	{
+		if (v[i].getterId() == a.getterId())
+		{
+			v.erase(v.begin() + i);
+			return;
+		}
+		i++;
+	}
+}
+
+void Rental::display_clients()
+{
+	int i = 0;
+	while (i < l.size())
+	{
+		cout << v[i].getterId() << " " << v[i].getterName() << endl;
+		i++;
+	}
+}
+
+void Rental::update_client(Kunde a, string newname)
+{
+	int i = 0;
+	while (i < v.size())
+	{
+		if (v[i].getterId() == a.getterId())
+		{
+			v[i].setterName(newname);
+		}
+		i++;
+	}
